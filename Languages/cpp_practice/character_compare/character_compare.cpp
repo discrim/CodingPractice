f6200c03821aaf9e0acf0660b6f5af4b2d8ecd9f
discrim <junkeunp@umich.edu>
2020-10-24 03:09:19 -0400

    C++ character compare with == operator

---
+++ Languages/cpp_practice/character_compare/character_compare.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+  string s1 = "abcde";
+  if (s1[1] == 'c') cout << "Same" << endl;
+  else cout << "Different" << endl;
+  return 0;
+}
